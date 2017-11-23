5e351f9bd720483f12e169afab83ffd377359df6
Guillermo Mata <mata.ruiz.g@gmail.com>
2017-11-23 13:05:02 +0100

    Scripts python copia seguridad

---
+++ copia.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+
+import os
+usu = raw_input("Usuario: ")
+pas = raw_input("Password: ")
+bas = raw_input("Base de Datos: ")
+
+os.system('mysqldump --user=' + usu + ' --password=' + pas + ' ' + bas + '> copia-' + bas + '.sql | tar -cvzf comprimido-' + bas + '-$(date +%d%m%y-%H%M).tar'' ./copia-' + bas + '.sql')
